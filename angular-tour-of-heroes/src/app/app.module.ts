60800500788cce65f335797c7f409361d3d8b4f4
George <gidiaconu@live.com>
2023-06-01 09:58:23 +0000

    Add the AppRoutingModule

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
@@ -19,6 +20,8 @@ import { MessagesComponent } from './messages/messages.component'; // <-- NgMode
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

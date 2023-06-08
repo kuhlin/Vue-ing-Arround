6f8ab8e4fa84a722abd0ae2fe7d607690ad15654
George <gidiaconu@live.com>
2023-06-08 13:39:39 +0000

    Seting up the server

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -16,7 +17,8 @@ import { MessagesComponent } from './messages/messages.component'; // <-- NgMode
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
